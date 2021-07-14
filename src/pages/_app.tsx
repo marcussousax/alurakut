21851c3e90e38ca917e0bc123ccd51cf29558213
Marcus Sousa <marvinsousa83@gmail.com>
2021-07-14 13:04:05 -0300

    moving files to SRC folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import type { AppProps } from 'next/app'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    background-color: #D9E6F6;
+    font-family: sans-serif;
   }
 `
 
