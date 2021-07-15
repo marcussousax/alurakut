f4cf017f68257d5c409d9e4ea22c13d1badb75c8
Marcus Sousa <marvinsousa83@gmail.com>
2021-07-15 10:51:50 -0300

    Center the mains content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,29 +15,31 @@ export default function Home() {
     return (
         <>
             <Header user={user} />
-            <MainGrid>
-                <GridArea areaName="profileArea">
-                    <Profile user={user} />
-                </GridArea>
-                <GridArea areaName="welcomeArea">
-                    <Box css={`padding: 24px`}>
-                        <h1 className={'title'}>Bem-vindo(a), {user}</h1>
-                        <p>Sorte de hoje: O melhor profeta do futuro é o passado</p>
-                        <OrkutNostalgicIconSet />
-                    </Box>
-                </GridArea>
-                <GridArea areaName="profileRelationsArea">
-                    <Box>
-                        <p>profileRelationsArea</p>
-                    </Box>
-                </GridArea>
-            </MainGrid>
+            <MainGridWrapper>
+                <MainGrid>
+                    <GridArea areaName="profileArea">
+                        <Profile user={user} />
+                    </GridArea>
+                    <GridArea areaName="welcomeArea">
+                        <Box css={`padding: 24px`}>
+                            <h1 className={'title'}>Bem-vindo(a), {user}</h1>
+                            <p>Sorte de hoje: O melhor profeta do futuro é o passado</p>
+                            <OrkutNostalgicIconSet />
+                        </Box>
+                    </GridArea>
+                    <GridArea areaName="profileRelationsArea">
+                        <Box>
+                            <p>profileRelationsArea</p>
+                        </Box>
+                    </GridArea>
+                </MainGrid>
+            </MainGridWrapper>
         </>
     )
 }
 
 const MainGrid = styled.main`
-
+  max-width: 1110px;
   gap: 10px;
   padding: 16px;
   flex: 1;
@@ -47,4 +49,10 @@ const MainGrid = styled.main`
     grid-template-areas: "profileArea welcomeArea profileRelationsArea";
     grid-template-columns: 160px 1fr 312px;
   }
+`
+const MainGridWrapper = styled.section`
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
 `
