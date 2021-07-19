8b65eb95382c8aec6266f6ff96715ea8c0743902
Marcus Sousa <marvinsousa83@gmail.com>
2021-07-18 23:18:54 -0300

    Install and Configure DatoCMS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     reactStrictMode: true,
+    env: {
+        DATOCMS_TOKEN: process.env.DATOCMS_TOKEN,
+    },
     images: {
-        domains: ['avatars.githubusercontent.com', 'github.com', 'picsum.photos']
+        domains: ['avatars.githubusercontent.com', 'www.alura.com.br', 'alurakut.vercel.app', 'github.com', 'picsum.photos']
     }
 }
